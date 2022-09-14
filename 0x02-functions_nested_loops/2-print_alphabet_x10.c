3ea043c52316858e798cc5f0d962cd8bb64147c9
Mark Nwabueze <markdlifer9@gmail.com>
2022-09-13 18:20:06 -0700

    e no easy

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print-alphabet_x10 - print alphabets in lower cases
+ * description: print 10 timws
+ * Return: Always 0(success)
+ */
+
+void print_alphabet_x10(void)
+{
+
+	int alphabet_count = 0;
+	while (alphabet_count < 10)
+	{
+		char ch = 'a';
+
+		     while (ch <= 'z')
+		     {
+			     _putchar(ch);
+			     ch++;
+		     }
+		alphabet_count++;
+		_putchar('\n');
+				}
+}
